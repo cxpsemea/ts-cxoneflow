a1c8053db9333169a91b4e93ba84e2204c6ca687
JoaoCosta <jcostate@gmail.com>
2024-01-13 15:59:49 +0000

    CxOneFlowManifetsWindows

--- .build/make-version.bat
+++ .build/make-version.bat
@@ -3,5 +3,5 @@ pushd "%~dp0"
 :: Apply version to python about file
 echo version = { 'version': '%1' } > ..\shared\version.py
 :: Apply version to manifests
-powershell -NoProfile -Command "$x = Get-Content -Encoding UTF8 '..\cxoneflow\src\cxinventorymanifestwindows.yaml'; $x[0] = 'Version: %1'; $x | Out-File -Encoding UTF8 '..\cxoneflow\src\cxoneflowmanifestwindows.yaml'"
+powershell -NoProfile -Command "$x = Get-Content -Encoding UTF8 '..\cxoneflow\src\cxoneflowmanifestwindows.yaml'; $x[0] = 'Version: %1'; $x | Out-File -Encoding UTF8 '..\cxoneflow\src\cxoneflowmanifestwindows.yaml'"
 popd
