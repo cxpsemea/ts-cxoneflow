b3cb9f5ac0c9e460c1900c0a89876dc871a09c2f
JoaoCosta <jcostate@gmail.com>
2024-01-13 15:37:30 +0000

    Builder git-hub-action

--- .build/build-windows.bat
+++ .build/build-windows.bat
@@ -4,13 +4,13 @@ pushd "%~dp0"
 :: -----------------------------
 :: Create cxinventory executable
 :: -----------------------------
-create-version-file ..\cxoneflow\src\cxoneflowfestwindows.yaml --outfile cxoneflowmanifestwindows.txt
+create-version-file ..\cxoneflow\src\cxoneflowmanifestwindows.yaml --outfile cxoneflowmanifestwindows.txt
 pyinstaller --clean --noconfirm --onefile --nowindow --distpath=..\.dist\cxoneflow\windows --workpath=temp --paths=..\shared --version-file=cxoneflowmanifestwindows.txt --icon=..\shared\imaging\icon.ico ..\cxoneflow\cxoneflow.py
 copy ..\cxoneflow\src\cxoneflowapplication.yaml ..\.dist\cxoneflow\windows\application.yaml
 copy ..\LICENSE ..\.dist\cxoneflow\windows\LICENSE
 del cxoneflowmanifestwindows.txt
 del cxoneflow.spec
 rmdir /s /q temp
-powershell Compress-Archive -Force -CompressionLevel Optimal -Path ..\.dist\cxoneflow\windows\*.* ..\.dist\cxoneflow-win64.zip
+powershell Compress-Archive -Force -CompressionLevel Optimal -Path ..\.dist\cxoneflow\windows\* ..\.dist\cxoneflow-win64.zip
 
 popd
